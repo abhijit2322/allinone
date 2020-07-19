d2d4a85f1422b5ca02ff8c10bed99ba6cccfca62
Abhijit Sarkar <abhijit@Abhijits-MacBook-Pro.local>
2020-07-19 22:51:07 +0530

    updated source code

--- app/src/main/java/com/abhijit/allinone/AppGlobalSetting.java
+++ app/src/main/java/com/abhijit/allinone/AppGlobalSetting.java
@@ -4,22 +4,22 @@ public class AppGlobalSetting {
 
 
     public static Integer[] thumbImages = {
-            R.drawable.food, R.drawable.gym,
-            R.drawable.gym, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food,R.drawable.food
+            R.drawable.food, R.drawable.career,
+            R.drawable.gym, R.drawable.ent_icon,
+            R.drawable.family_time, R.drawable.friends,
+            R.drawable.audiance, R.drawable.android,
+            R.drawable.idea, R.drawable.lovely_time,
+            R.drawable.medical, R.drawable.me_time,
+            R.drawable.paintbrush, R.drawable.playstore,
+            R.drawable.settings, R.drawable.star,
+            R.drawable.suggestion, R.drawable.team_time,
+            R.drawable.person1, R.drawable.person2,
+            R.drawable.person3, R.drawable.trash,
+            R.drawable.ent2_icon, R.drawable.ent3_icon,
+            R.drawable.calendar, R.drawable.food,
+            R.drawable.tickcircle, R.drawable.tickinsidecircle,
+            R.drawable.food, R.drawable.career,
+            R.drawable.gym,R.drawable.ent_icon
     };
 
     public static String[] imagefooter = {
